87a4a2468ba96f59ae7764e3534f8a05c341d48e
David <Davidmaxsands@gmail.com>
2022-03-17 14:37:48 -0600

    commit tree c71134831dc4a0f0d8cefbedc8a699513f00c38d

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show 
+    item = Item.find(params[:id])
+    render json: item 
+  end
+
+  def create 
+      user = find_user
+      item = user.items.create!(item_params)
+      render json: item, status: 201
+  end
+  
+  private 
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def item_params 
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def user_not_found
+    render json: { error: "User not found" }, status: 404
   end
 
 end
